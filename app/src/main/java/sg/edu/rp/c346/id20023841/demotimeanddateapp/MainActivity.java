feff3a870e8ae53b16bed415be4d865a321f9e0a
20023841 <20023841@myrp.edu.sg>
2021-05-10 11:20:44 +0800

    Worksheet Project 1 Update

--- app/src/main/java/sg/edu/rp/c346/id20023841/demotimeanddateapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20023841/demotimeanddateapp/MainActivity.java
@@ -3,12 +3,61 @@ package sg.edu.rp.c346.id20023841.demotimeanddateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
+    DatePicker dp;
+    TimePicker tp;
+    Button btnDisplayDate, btnDisplayTime, btnReset;
+    TextView tvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime = findViewById(R.id.buttonDisplayTime);
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnReset = findViewById(R.id.buttonReset);
+
+        btnDisplayTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Integer hour = tp.getCurrentHour();
+                Integer minute = tp.getCurrentMinute();
+                tvDisplay.setText("Time is " + hour + ":" + minute);
+            }
+        });
+
+        btnDisplayDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int day = dp.getDayOfMonth();
+                int month = dp.getMonth() +1;
+                int year = dp.getYear();
+
+                tvDisplay.setText("Date is " + day + "/" + month + "/" + year);
+            }
+        });
+
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tp.setCurrentHour(0);
+                tp.setCurrentMinute(0);
+                dp.updateDate(2020,0, 1 );
+            }
+        });
+
+
+
     }
 }
